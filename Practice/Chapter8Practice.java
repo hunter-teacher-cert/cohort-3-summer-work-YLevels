1f1ee1d5a1f7d928568328bab971e89c165b55ca
YLevels <103545784+YLevels@users.noreply.github.com>
2022-07-04 19:15:39 +0000

    Uploaded and practiced concepts in Chapter 8 of CSAwesome.

---
+++ Practice/Chapter8Practice.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+//Chapter 8 CS Awesome 2D Arrays
+public class Chapter8Practice 
+{
+
+  //getAverage method computes the average of all the values within any integer array. It uses a nested for loop (one loop inside of another loop) to loop/traverse through all of the elements of the 2D array. 
+  public static double getAverage (int[][] a) 
+  {
+    double total = 0;
+    int value = 0;
+    for (int row = 0; row < a.length; row++) 
+    {
+      for (int col = 0; col < a[0].length; col++) 
+      {
+        value = a[row][col];
+        total = total + value;
+      }  
+    }
+    return total / (a.length * a[0].length);
+  }
+
+  //getLargest method traverases through all of the elements of the 2D array 
+  public static int getLargest(int[][] a)
+  {
+    int largest = a[0][0];
+    int current = 0;
+    for (int r = 0; r < a.length; r++) 
+    {
+      for (int c = 0; c < a[0].length; c++) 
+      {
+        current = a[r][c];
+        if (current > largest) 
+        {
+          largest = current;
+        } //end if
+      } //end column loop
+    } //end row loop
+    return largest;
+  } //end method
+  
+  public static void main (String[] args) 
+  {
+    int [][] matrix = { {1,2,3},{4,5,6}};
+    System.out.println(getAverage(matrix));
+    System.out.println(getLargest(matrix));
+  }
+  
+}
